d89de89dbcf7fc0693653634b149d99b88c7f995
Frol <s.frol173@gmail.com>
2024-07-31 18:55:36 +0200

    header done

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   linksContainer.classList.toggle("active");
 });
+
+//get price
+
+const buttonPrice = document.querySelector(".btn-price");
+const buttonPriceClose = document.querySelector(".button-price-close");
+const modalWrapper = document.querySelector(".price-modal");
+
+modalWrapper.style.display = "none";
+buttonPrice.addEventListener("click", () => {
+  modalWrapper.style.display = "block";
+  document.body.classList.add("disable-scroll");
+});
+buttonPriceClose.addEventListener("click", () => {
+  modalWrapper.style.display = "none";
+  document.body.classList.remove("disable-scroll");
+});
