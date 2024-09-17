86485b5a450fd138730c388480e97d4dfd0e1eae
Frol <s.frol173@gmail.com>
2024-09-17 18:02:07 +0200

    Homepage Done

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const buttonPrice = document.querySelector(".btn-price");
 const buttonPriceClose = document.querySelector(".button-price-close");
 const modalWrapper = document.querySelector(".price-modal");
 
-modalWrapper.style.display = "none";
 buttonPrice.addEventListener("click", () => {
   modalWrapper.style.display = "block";
   document.body.classList.add("disable-scroll");
@@ -24,3 +23,71 @@ buttonPriceClose.addEventListener("click", () => {
   modalWrapper.style.display = "none";
   document.body.classList.remove("disable-scroll");
 });
+
+//header scroll
+
+const headerWrapper = document.querySelector(".header__wraper");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    headerWrapper.classList.add("header-bg-scroll");
+  } else if (window.scrollY < 30) {
+    headerWrapper.classList.remove("header-bg-scroll");
+  }
+});
+
+//append later
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => {
+  observer.observe(el);
+});
+const hiddenElements2 = document.querySelectorAll(".hidden-2");
+hiddenElements2.forEach((el) => {
+  observer.observe(el);
+});
+const hiddenElements3 = document.querySelectorAll(".hidden-3");
+hiddenElements3.forEach((el) => {
+  observer.observe(el);
+});
+
+//slide gallery
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active-dot";
+}
